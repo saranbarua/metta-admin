f8f7331631271d21141440f73bc68b1fc80d9f9b
saranbarua <saranbarua2357@gmail.com>
2025-09-30 11:28:17 +0600

    url changed

--- src/apiurl/apiurl.js
+++ src/apiurl/apiurl.js
@@ -1,6 +1,6 @@
 const apiurl = {
-  mainUrl: "http://backend.mettadhamma.com/api",
-  imgUrl: "http://backend.mettadhamma.com",
+  mainUrl: "https://backend.mettadhamma.com/api",
+  imgUrl: "https://backend.mettadhamma.com",
   // mainUrl: "http://localhost:5000/api",
 };
 
